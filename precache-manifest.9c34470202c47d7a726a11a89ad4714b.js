94940bb29bcf607c1e9c3f9c40d427aa373879c1
yoieh <Yoieh@live.se>
2021-12-19 01:18:32 +0000

    Deploying to gh-pages from @ yoieh/use-canvas-camera2d@bf72eca74976ac3b8cdafbc3beef179b284e4671 🚀

---
+++ precache-manifest.9c34470202c47d7a726a11a89ad4714b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361289c8449a358198d192519a2f6218",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea969083edf9d626a6cb",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "2c252e2426b30601aecd",
+    "url": "./static/js/2.47f5536f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "./static/js/2.47f5536f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea969083edf9d626a6cb",
+    "url": "./static/js/main.b1771634.chunk.js"
+  },
+  {
+    "revision": "449f690c90764879c4eb",
+    "url": "./static/js/runtime-main.251c49f0.js"
+  }
+]);
